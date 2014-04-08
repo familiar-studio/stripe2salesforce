0ae6720bcafad20c5d7b46c826f7427917f8b9bc
iwoodruff <isaac.woodruff@gmail.com>
2014-04-08 15:24:20 -0400

    debugging console.logs added

--- web.js
+++ web.js
@@ -10,24 +10,25 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-var testJson = {
-	head: "hello, this is a test",
-	body: "hi, Meghann",
-	object: {
-		head: "this is a nested test",
-		body: "written by isaac and meghann"
-	}
-};
+// var testJson = {
+// 	head: "hello, this is a test",
+// 	body: "hi, Meghann",
+// 	object: {
+// 		head: "this is a nested test",
+// 		body: "written by isaac and meghann"
+// 	}
+// };
 
 app.get('/', function(req, res) {
   // fs.appendFile('test.txt', JSON.stringify(testJson), function(err){
   // 	if (err) { console.log(err) } else { console.log('worked!') }
   // });
+	console.log('TEST >>>>>>>>>>>>>>>>> this is the console')
 	res.send('server is running');
 });
 
 app.post('/stripe.webhook', function(request, response) {
-	console.log(request);
+	console.log("TEST =================", request);
 	// if (request.body.type === 'charge.succeded') {
 	// 	fs.appendFile('test.txt', JSON.stringify(request.body.data.object)), function(err) {
 	// 	if (err) {
