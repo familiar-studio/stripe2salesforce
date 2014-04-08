68841596c8f0144c617d441d73e487961b918b0b
iwoodruff <isaac.woodruff@gmail.com>
2014-04-08 19:43:50 -0400

    updated comments.

--- web.js
+++ web.js
@@ -6,38 +6,44 @@ var EventEmitter = require('events').EventEmitter;
 var stripe = require('stripe');
 
 
-
+// =========================================
+// MONGO REQUIREMENTS & VARIABLES
 
 var mongo = require('mongodb');
-var databaseUrl = 'db.js'
+var databaseUrl = 'stripeLogs.js'
 var collection = ['stripeReq']
 
 var db = mongo.connect(databaseUrl, collection)
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:5000/db'
 
+// =========================================
 
-// stripe.setApiKey('sk_test_bY22es5dN0RpWmJoJ5VlBQ5E')
 
 var emitter = new EventEmitter;
 var app = express();
 
 app.use(express.bodyParser());
-
 app.use(logfmt.requestLogger());
-
-
-
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:5000/db'
 
 
 app.get('/', function(req, res) {
 	res.send('server is running');
 
-	mongo.Db.connect(mongoUri, function(err, db) {
-		console.log("DB===================", db)
-		db.collection('db', function(er, collection) {
-			collection.insert({'stripeReq':request.body})
-		})
-	});
+// =========================================
+// BROKEN DB INSERT HERE
+
+	// mongo.Db.connect(mongoUri, function(err, db) {
+	// 	console.log("DB===================", db)
+	// 	db.collection('db', function(er, collection) {
+	// 		collection.insert({'stripeReq':request.body})
+	// 	})
+	// });
+
+// IT'S IN THE ROOT ROUTE NOW SO IT'S HIT ON
+// LOAD BUT WILL LIVE IN APP.POST EVENTUALLY
+// =========================================
+
+
 });
 
 app.post('/webhook', function(request, response){
