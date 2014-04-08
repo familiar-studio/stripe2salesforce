de5edc3b8f7d7fcff298a9fe48f288b2c585ac23
Plunkett112358 <plunkett.meghann@gmail.com>
2014-04-08 17:54:37 -0400

    testing test file

--- web.js
+++ web.js
@@ -32,7 +32,7 @@ app.get('/', function(req, res) {
 
 app.post('/webhook', function(request, response){
 	if (request.body.type === 'charge.succeeded') {
-		fs.appendFile('test.txt', request.body, function(err){
+		fs.appendFile('wow.txt', request.body, function(err){
 			if (err) {
 				console.log('error!', err)
 			} else {
@@ -49,6 +49,6 @@ app.post('/webhook', function(request, response){
 });
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function() 
+{  console.log("Listening on " + port);
 });
