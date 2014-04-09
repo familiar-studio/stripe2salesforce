7ff0d9154d362e851f7f42c95e0f1573c930aff5
Plunkett112358 <plunkett.meghann@gmail.com>
2014-04-09 13:50:45 -0400

    card details

--- web.js
+++ web.js
@@ -50,10 +50,14 @@ app.get('/', function(req, res) {
 
 app.post('/webhook', function(request, response){
   //grabbing customer object
+  console.log("THIS IS THE CARD DEATILS*******", request.body.data.object.card)
   // console.log("THIS IS THE ID_____", (request.body.data.object.customer))
   stripe.customers.retrieve(request.body.data.object.customer, function(err, customer) {
 
     console.log('********************************THIS IS IT __________EMAIL', customer.email)
+    //if no email logges as null
+
+
     // console.log('********************************THIS IS IT _______ID', customer.id)
 });
 	// on post from stripe webhook, dump json transaction in mongodb
