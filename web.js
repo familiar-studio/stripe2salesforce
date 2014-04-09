1cb21f0067dc37b8c6f5cac861953d476775dc38
Keith Mancuso <keithmancuso@gmail.com>
2014-04-09 09:53:47 -0400

    added saleforce

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var logfmt = require("logfmt");
 var fs = require('fs');
 var EventEmitter = require('events').EventEmitter;
 var stripe = require('stripe');
+var jsforce = require('jsforce');
 
 
 
-
-// var mongo = require('mongo');
+var mongo = require('mongodb');
 // var databaseUrl = 'stripeLogs'
 // var collection = ['stripeReq']
 
@@ -22,24 +22,43 @@ var app = express();
 
 app.use(express.bodyParser());
 
+app.use(app.router);
+
 app.use(logfmt.requestLogger());
 
 
+// Salesforce Connection information
+var conn = new jsforce.Connection({
+  oauth2 : {
+    clientId : '3MVG9y6x0357HleeZ5WRMCv.Ih7Uxos6mg6Y.7N3RdXzC15h..L4jxBOwzB79dpcRSxwpV3.OgbNXSSJiobQQ',
+    clientSecret : '8923954381316425368',
+    redirectUri : 'https://stripe2salesforce.herokuapp.com',
+    proxyUrl: 'https://pure-bastion-9629.herokuapp.com/proxy'
+
+  },
+  proxyUrl: 'https://pure-bastion-9629.herokuapp.com/proxy'
+});
+
+conn.login('keith@familiar-studio.com', 'KVWVXbwYUjbB33yDyh84HkGeL1fbW2ZDx0rnmu', function(err, res) {
+  if (err) { return console.error(err); }
+});
 
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost:5000/stripeLogs'
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:5000/stripeLogs'
 
 
 
 app.get('/', function(req, res) {
+	// mongo.Db.connect(mongoUri, function(err, db) {
+	// 	console.log(db)
+	// 	db.collection('stripeLogs', function(er, collection) {
+	// 		collection.insert({'stripeReq':request.body})
+	// 	})
+	// });
 
 
-	mongo.Db.connect(mongoUri, function(err, db) {
-		console.log(db)
-		db.collection('stripeLogs', function(er, collection) {
-			collection.insert({'stripeReq':request.body})
-		})
-	});
+
 });
 
 app.post('/webhook', function(request, response){
@@ -60,7 +79,27 @@ app.post('/webhook', function(request, response){
 	response.end()
 });
 
+
+app.get('/salesforce/read', function(request, response) {
+  console.log('Read!' );
+  conn.query('SELECT Id, Name, Phone FROM Account limit 10', function(err, res) {
+    if (err) { return console.error(err); }
+    console.log(res);
+  });
+
+});
+
+app.get('/salesforce/insert', function(request, response) {
+  console.log('Insert!' );
+  conn.sobject("Account").create({ Name : 'Test Account #1' }, function(err, ret) {
+    if (err || !ret.success) { return console.error(err, ret); }
+    console.log("Created record id : " + ret.id);
+    // ...
+  });
+
+});
+
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() 
+app.listen(port, function()
 {  console.log("Listening on " + port);
 });
