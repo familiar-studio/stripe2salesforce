93f62a34879ada42e77d7345171875087f217533
Plunkett112358 <plunkett.meghann@gmail.com>
2014-04-11 15:51:08 -0400

    typo

--- web.js
+++ web.js
@@ -104,7 +104,7 @@ app.post('/webhook', function(request, response){
 	}
 
 
-	var createSFOpportunity = function(stripe_info, ){
+	var createSFOpportunity = function(stripe_info){
 		var stripe_id = request.body.data.object.customer
 		var amount = request.body.data.object.amount
 		conn.sobject("Opportunity").create({ 
