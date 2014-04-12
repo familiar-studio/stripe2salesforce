63ca86885588d03dcc302191622b818c0a371fb7
iwoodruff <isaac.woodruff@gmail.com>
2014-04-11 22:28:34 -0400

    revert

--- web.js
+++ web.js
@@ -105,43 +105,62 @@ app.post('/webhook', function(request, response){
 	}
 
 
-	var createSFOpportunity = function(charge, contract_num){
-		console.log(charge)
-		var stripe_id = request.body.data.object.id
-		var amount = request.body.data.object.amount/100
-		var date = moment.unix(charge.created).format("YYYY-MM-DDTHH:mm:ss:ZZ")
+	// var createSFOpportunity = function(charge, contract_num){
+	// 	console.log(charge)
+	// 	var stripe_id = request.body.data.object.id
+	// 	var amount = request.body.data.object.amount/100
+	// 	var date = moment.unix(charge.created).format("YYYY-MM-DDTHH:mm:ss:ZZ")
 
-		if (contract_num) {
-			conn.sobject("Opportunity").create({ 
-				Amount: (charge.amount / 100),
-				Stripe_Charge_Id__c: charge.id + "SUBSCRIPTION", 
-				// name is currently fake for debugging purposes
-				Name: charge.id,
-				StageName: "Closed Won",
-				CloseDate: date,
-				Contract__c: contract_num
+	// 	if (contract_num) {
+	// 		conn.sobject("Opportunity").create({ 
+	// 			Amount: (charge.amount / 100),
+	// 			Stripe_Charge_Id__c: charge.id + "SUBSCRIPTION", 
+	// 			// name is currently fake for debugging purposes
+	// 			Name: charge.id,
+	// 			StageName: "Closed Won",
+	// 			CloseDate: date,
+	// 			Contract__c: contract_num
 
-			}, function(error, ret){
-				if (err || !ret.success) { return console.error(error, ret); }
-				console.log("Created Opportunity: " + ret);
-			});
-		} 
+	// 		}, function(error, ret){
+	// 			if (err || !ret.success) { return console.error(error, ret); }
+	// 			console.log("Created Opportunity: " + ret);
+	// 		});
+	// 	} 
 
-		conn.sobject("Opportunity").create({ 
-			Amount: amount, 
-			Stripe_Charge_Id__c: stripe_id, 
-			// name is currently fake for debugging purposes
-			Name: charge.id + 'SINGLE PAYMENT',
-			StageName: "Closed Won",
-			CloseDate: date
+	// 	conn.sobject("Opportunity").create({ 
+	// 		Amount: amount, 
+	// 		Stripe_Charge_Id__c: stripe_id, 
+	// 		// name is currently fake for debugging purposes
+	// 		Name: charge.id + 'SINGLE PAYMENT',
+	// 		StageName: "Closed Won",
+	// 		CloseDate: date
 
-		}, function(error, ret){
-			if (err || !ret.success) { return console.error(error, ret); }
-			console.log("Created Opportunity: " + ret);
-		});
+	// 	}, function(error, ret){
+	// 		if (err || !ret.success) { return console.error(error, ret); }
+	// 		console.log("Created Opportunity: " + ret);
+	// 	});
 
-		// TODO: add charge logic to checkName func
-	}
+	// 	// TODO: add charge logic to checkName func
+	// }
+
+	var createSFOpportunity = function(stripe_info){
+	  		var stripe_id = request.body.data.object.id
+	  		var amount = request.body.data.object.amount
+	  		// console.log('THIS IS THE AMOUT *********************', request.body.data.object.amount)
+	  		// console.log('THIS IS THE id *********************', request.body.data.object.customer)
+	  
+	  		conn.sobject("Opportunity").create({ 
+	  			Amount: amount, 
+	  			Stripe_Charge_Id__c: stripe_id, 
+	  			Name: "OUR Stripe Charge",
+	  			StageName: "Closed Won",
+	  			CloseDate: "2011-02-13T20:30:00.000Z"
+	  		
+	  		}, function(error, ret){
+	  			if (err || !ret.success) { return console.error(err, ret); }
+	  			console.log("created!!!!!!!!!!!! record id :" + ret.id);
+	  		});
+	  	}
  	
  	var findStripeSubscription = function(charge){
  		stripe.invoices.retrieve( charge.invoice, function(err, response){
@@ -180,7 +199,6 @@ app.post('/webhook', function(request, response){
 
  	var findSFContract = function(charge, contract_id) {
  		conn.sobject('Contract').find({ 'Id' : contract_id }).limit(1).execute(function(err, ret) { 
- 			console.log('YOU MIGHT BE BREAKING HERE', ret[0].ContractNumber)
  			createSFOpportunity(charge, ret[0].ContractNumber)
  		})
  	}
