e1e07fff9240dcbbcc9a68c8d949ae4a123e0211
Plunkett112358 <plunkett.meghann@gmail.com>
2014-04-14 15:12:48 -0400

    missing token

--- web.js
+++ web.js
@@ -41,7 +41,7 @@ var conn = new jsforce.Connection({
 
 
 
-conn.login('keith@familiar-studio.com', 'KVWVXbwYUjbB33yDyh84HkGeL1fbW2ZDx0rnmu', function(err, res) {
+conn.login('keith@familiar-studio.com', '5Mot26qP5mZAKlkZCyTIXSIE4', function(err, res) {
   if (err) { return console.error(err); }
 })
 
