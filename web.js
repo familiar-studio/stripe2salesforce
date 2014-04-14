ab0717f189868f2c83a8934a15ce2f94c30941ca
Plunkett112358 <plunkett.meghann@gmail.com>
2014-04-14 17:23:01 -0400

    beginning chaining

--- web.js
+++ web.js
@@ -147,10 +147,29 @@ app.post('/webhook', function(request, response){
 
 
 		if (request.body.type === 'charge.succeeded') {
+			var chargeObj = request.body.data.object
+			var acct_id
  			// WAIT UNTIL INVOKED BY CUSTOMER VALIDATION
  			var stripe_id = request.body.data.object.customer;
  			console.log("STRIPE ID", stripe_id)
- 			stripeId2SalesContact(stripe_id).then(function(){salesContact2Account(stripe_id)})
+ 			stripeId2SalesContact(stripe_id).then(function(){
+ 				acct_id = salesContact2Account(stripe_id)
+ 				console.log("POST ACCT FETCH", acct_id)
+
+ 			}).then(function(){
+ 				console.log("PRE OPPORTUNITY ACCT",acct_id)
+ 				var charge = request.body.data.object;
+ 				console.log(charge.invoice)
+ 				if (charge.invoice !== null) {
+ 					// stripeSubId2SFID
+ 					console.log('INVOICE EXISTS')
+
+ 				} else {
+ 					console.log('INVOICE == NULL')
+ 				};
+
+ 				// doSomthingWithId(acct_id)
+ 			})
 
  		};
 
