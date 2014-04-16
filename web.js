c5bb7e29b07f616ee60bc8a8e6aa7c5022e501e6
iwoodruff <isaac.woodruff@gmail.com>
2014-04-16 15:38:24 -0400

    didn't work

--- web.js
+++ web.js
@@ -338,7 +338,7 @@ var getLogins = function(organization){
 	mongo.Db.connect(mongoUri, function(err, db) {
 		db.collection('Organizations', function(er, organizations){
 			if (er) { console.log(er); } console.log('in collection')
-			organizations.find({ "organization" }, function(error, result){
+			organizations.find("organization", function(error, result){
 				console.log("THIS IS THE RESULT :", result)
 			})
 		})
